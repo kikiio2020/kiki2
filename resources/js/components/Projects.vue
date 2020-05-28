29174ca2d964af40733a2b406f2b59122a2a05e7
Daniel 'Kikiio' Leung <daniel@kikiio.com>
2020-05-28 11:51:24 -0700

    Add vue-crud-control doc + composer update + upm update

--- resources/js/components/Projects.vue
+++ resources/js/components/Projects.vue
@@ -2,6 +2,7 @@
 <b-container>
 <b-row><b-col class="text-left">
 <h2>Vue Plugins</h2>
+
 <b-card title="VueFlippingStepper" class="bg-transparent">
 <b-card-text>
 A simple horizontally rotating stepper for use in a Vue-Bootstrap project. 
@@ -10,6 +11,16 @@ A simple horizontally rotating stepper for use in a Vue-Bootstrap project.
 <a href='/project/vue-flipping-stepper' target="_blank" class="text-white">Learn more</a>
 </b-card-text>
 </b-card>
+
+<b-card title="VueCrudControl" class="bg-transparent">
+<b-card-text>
+A data table that loads a GET API end point and provision UI elements for the other CRUD actions. 
+</b-card-text>
+<b-card-text>
+<a href='/project/vue-crud-control' target="_blank" class="text-white">Learn more</a>
+</b-card-text>
+</b-card>
+
 </b-col></b-row>
 </b-container>
 
